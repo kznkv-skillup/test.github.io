8d10a980467d155a6be03d4385db6fc9981234ca
Ania <anianebesna@outlook.com>
2019-06-24 20:33:15 +0300

    add shop project

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var productsCountEl = document.getElementById("products-count");
 var addToCartButtons = document.querySelectorAll (".add-to-cart");
 
 for(var i=0; i<addToCartButtons.length; i++) {
-	addToCartButtons [i].addEventListener ("click", function (){
+	addToCartButtons[i].addEventListener ("click", function (){
 		console.log ("clicked")
 		var prevProductsCount = +productsCountEl.textContent
 		productsCountEl.textContent = prevProductsCount + 1
@@ -11,8 +11,25 @@ for(var i=0; i<addToCartButtons.length; i++) {
 	});
 }
 
+// toggle like state
 
- $('div.like').click(function() {
-    $(this).toggleClass('changeColorLike');
-  });
+var likeButtons = document.querySelectorAll(".like")
+
+for(var i = 0; i<likeButtons.length;i++) {
+	likeButtons[i].addEventListener('click',function() {
+		if(this.classList.contains("liked")) {
+			this.classList.remove('liked')
+		} else {
+			this.classList.add('liked')
+		}
+	})
+}
+
+//add slick
+
+$('.slider-block').slick({
+	dots: true,
+});
+
+
 
